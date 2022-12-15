9fcfd091c91d31f43424e101212d71ab7c3cdae6
Oseas Neto <105180872+Oseasneto@users.noreply.github.com>
2022-12-14 23:29:02 -0300

    Add files via upload

--- src/main/java/oo/heranca/Jogo.java
+++ src/main/java/oo/heranca/Jogo.java
@@ -3,13 +3,21 @@ package oo.heranca;
 public class Jogo {
     public static void main(String[] args) {
 
-        Jogador j1 = new Jogador();
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.NORTE);
+        Monstro j1 = new Monstro();
+        j1.x = 10;
+        j1.y = 10;
 
-        System.out.println(j1.y);
+        Heroi j2 = new Heroi();
+        j2.x = 10;
+        j2.y = 11;
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
+
+        j1.atacar(j2);
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
 
     }
 }
